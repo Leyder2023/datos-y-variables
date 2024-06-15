1ed2475cc5fce4830c2050c4f878440535e09762
Leyder2023 <151747360+Leyder2023@users.noreply.github.com>
2024-06-14 23:43:15 -0500

    Add files via upload

---
+++ datosYVariables1/src/main/java/com/mycompany/datosyvariables1/DatosYVariables1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.datosyvariables1;
+
+/**
+ *
+ * @author john
+ */
+public class DatosYVariables1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ datosYVariables2/src/main/java/com/mycompany/datosyvariables2/DatosYVariables2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.datosyvariables2;
+
+/**
+ *
+ * @author john
+ */
+public class DatosYVariables2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
